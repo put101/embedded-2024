c797982bcadceb58c8e2cefb155e61476366da5d
put101 <tobiaspucher@gmail.com>
2024-06-04 14:17:37 +0200

    test

--- python-wheel.py
+++ python-wheel.py
@@ -4,10 +4,13 @@
 # - [] Send data to the cloud
 
 import paho.mqtt.client as mqtt
-  
+
+client = mqtt.Client()
+client.on_connect = on_connect
 
 def on_connect(client, userdata, flags, rc):
     print("Connected with result code "+str(rc))
     client.subscribe("sensor-data")
     
-    
+client.connect("mqtt.pervasive.org", 1883, 60)
+
